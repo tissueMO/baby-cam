02937da43179f3b92a6c537fedb70265e5bd6836
tissueMO <ts32worker@gmail.com>
2022-01-31 21:00:33 +0900

    配信サーバーとWebサーバーを暫定的に整備

--- web/contents/js/main.js
+++ web/contents/js/main.js
@@ -1,11 +1,14 @@
-const video = document.getElementById('video');
 const videoUrl = '/live/stream.m3u8';
 
-if (Hls.isSupported()) {
-  const hls = new Hls();
-  hls.loadSource(videoUrl);
-  hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
-} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  video.src = videoUrl;
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const video = document.getElementById('video');
+
+  if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(videoUrl);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoUrl;
+  }
+});
