938361c8a6c8206f67d51a20afa5ebcfae731fff
tissueMO <ts32worker@gmail.com>
2022-02-03 00:16:29 +0900

    ビューアをBootstrap対応版に差し替え、コンテンツ拡充

--- web/contents/js/main.js
+++ web/contents/js/main.js
@@ -2,13 +2,32 @@ const videoUrl = '/live/stream.m3u8';
 
 document.addEventListener('DOMContentLoaded', () => {
   const video = document.getElementById('video');
+  const hls = new Hls();
+  const timer = null;
 
   if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(videoUrl);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+    loadStream(hls, video);
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      video.play();
+      if (timer !== null) {
+        clearInterval(timer);
+      }
+    });
+    hls.on(Hls.Events.ERROR, (_, data) => {
+      console.info('HLS-ERROR:', data);
+      if (['levelLoadError', 'manifestLoadError'].includes(data.details)) {
+        document.querySelector('.js-error').classList.remove('d-none');
+        if (timer === null) {
+          timer = setInterval(() => loadStream(hls, video), 5000);
+        }
+      }
+    });
   } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
     video.src = videoUrl;
   }
 });
+
+const loadStream = (hls, video) => {
+  hls.loadSource(videoUrl);
+  hls.attachMedia(video);
+};
