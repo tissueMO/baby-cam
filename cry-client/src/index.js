7f8576e4b6fd55af90a1a9c04a9e6533d4a5cb46
tissueMO <ts32worker@gmail.com>
2022-02-06 14:26:08 +0900

    マイクからの音量測定ができるようになった

--- cry-client/src/index.js
+++ cry-client/src/index.js
@@ -1,16 +1,44 @@
-const fs = require('fs');
-const portAudio = require('naudiodon');
+const { AudioIO, SampleFormat16Bit } = require('naudiodon');
+const pcm = require('pcm-util');
 
-const io = new portAudio.AudioIO({
+const scores = [];
+let currentTimestamp = null;
+
+const io = new AudioIO({
   inOptions: {
     channelCount: 1,
-    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleFormat: SampleFormat16Bit,
     sampleRate: 44100,
-    deviceId: 2,
-    closeOnError: false,
+    deviceId: 1,
+    closeOnError: false
   },
 });
 
-const stream = fs.createWriteStream('rec.raw');
-io.on('data', chunk => stream.write(chunk));
+io.on('data', (chunk) => {
+  // 1秒単位でバッファリング
+  if (chunk.timestamp !== currentTimestamp) {
+    currentTimestamp = chunk.timestamp;
+    console.log(scores);
+
+    // TODO: App へ scores を送信
+
+    scores.splice(0);
+  }
+
+  // 現在のチャンクの音量を計算
+  const buffer = pcm.toAudioBuffer(chunk).getChannelData(0);
+  const peak = Math.max(...buffer.map(d => Math.abs(d)));
+  if (!Number.isFinite(peak)) {
+    return;
+  }
+  const rms = Math.sqrt(
+    buffer
+      .map(d => d * d)
+      .reduce((sum, d) => sum + d, 0) / buffer.length
+  );
+  const db = 20 * Math.log10(rms / 20e-6);
+
+  scores.push({ db, rms, peak });
+});
+
 io.start();
