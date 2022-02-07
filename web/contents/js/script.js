111944e591c29407bd919c29d8fe91a8f8370ea3
tissueMO <ts32worker@gmail.com>
2022-02-08 02:07:26 +0900

    iOS Safariでの配信停止時の自動復旧に対応

--- web/contents/js/script.js
+++ web/contents/js/script.js
@@ -3,45 +3,53 @@
  */
 document.addEventListener('DOMContentLoaded', async () => {
   const videoUrl = '/live/stream.m3u8';
-  const video = document.getElementById('video');
   const hls = new Hls();
   let timer = null;
-
   console.info('HLSサポート:', Hls.isSupported());
+
+  /**
+   * @type HTMLVideoElement
+   */
+  const video = document.getElementById('video');
 
   // 再生成功時
   video.addEventListener('play', () => {
     document.querySelector('.js-error').classList.remove('show');
-    if (timer !== null) {
-      clearInterval(timer);
-    }
+    clearInterval(timer);
+    timer = null;
   });
 
-  // HLS非サポート環境 (iPhone Safari 向け)
-  if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoUrl;
-    return;
-  }
   // HLS非サポート環境 (再生不可)
-  if (!Hls.isSupported()) {
+  if (!Hls.isSupported() && !video.canPlayType('application/vnd.apple.mpegurl')) {
+    alert('非対応のブラウザーです。');
     return;
   }
 
-  // HLSサポート環境 (自動再復旧にも対応)
-  document.querySelector(`.js-hls-enabled`).classList.remove('d-none');
+  // HLS非サポート環境 (iOS Safari 向け)
+  if (!Hls.isSupported()) {
+    video.src = videoUrl;
+
+    // 配信不具合発生時に自動で再復旧
+    video.addEventListener('stalled', () => {
+      document.querySelector('.js-error').classList.add('show');
+      timer ??= setInterval(() => video.load(), 5000);
+    });
+
+    return;
+  }
+
+  // HLSサポート環境
   hls.loadSource(videoUrl);
   hls.attachMedia(video);
   hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
   hls.on(Hls.Events.ERROR, (_, data) => {
-    console.warn('HLS-ERROR:', data);
+    // 配信不具合発生時に自動で再復旧
     if (data.fatal) {
       document.querySelector('.js-error').classList.add('show');
-      if (timer === null) {
-        timer = setInterval(() => {
-          hls.loadSource(videoUrl);
-          hls.attachMedia(video);
-        }, 5000);
-      }
+      timer ??= setInterval(() => {
+        hls.loadSource(videoUrl);
+        hls.attachMedia(video);
+      }, 5000);
     }
   });
 });
